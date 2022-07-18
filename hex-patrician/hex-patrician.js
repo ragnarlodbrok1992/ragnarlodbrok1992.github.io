8f6006854731a7437be400d8d4e871b16bb4b4d0
ragnarlodbrok1992 <maraus92@gmail.com>
2022-07-18 22:22:04 +0200

    changes

---
+++ hex-patrician/hex-patrician.js
@@ -0,0 +1,8 @@
+console.log("This is Hex Patrician game - alpha version.");
+
+// Sort of main of javascript file
+(() => {
+    // Getting HTML canvas
+    var mainCanvas = document.getElementById('main-canvas');
+    var context2d = mainCanvas.getContext('2d');
+})();
