2fa8a522016564ffaf106d0191b8bd8c2a6bf50e
Maciej Ragnar Oliwa <maraus92@gmail.com>
2022-10-30 11:52:11 +0100

    Fixed asynchronous problem of my JS code

--- index.js
+++ index.js
@@ -4,42 +4,28 @@ import * as canvasConsole from "./canvas-console/canvas-console.js";
 // Global variables
 const main_content_file = './content/main.txt';
 
-async function get_string_from_file(file) {
-    const x = fetch(file)
-                .then((response) => {
-                    return response.text();
-                }).then((string) => {
-                    return string;
-                });
-    return x;
-}
-
-async function set_prompt(prompt) {
-    return new Promise((resolve) => {
-        canvasConsole.set_prompt(prompt);
-    });
-}
-
-// async function write_string(context, string) {
-    
-// }
-
 // Sort of main of javascript file
 (async () => {
-    // Main
-    var consoleCanvas = document.getElementById('console-canvas');
-    var context2d = consoleCanvas.getContext('2d');
+    fetch(main_content_file)
+        .then((response) => {
+            return response.text();
+        })
+        .then((string) => {
+            // Main
+            var consoleCanvas = document.getElementById('console-canvas');
+            var context2d = consoleCanvas.getContext('2d');
+            
+            // DEBUG - got string?
+            console.log("I got string from file here: ", string);
 
-    console.log("Before setting prompt");
-    await set_prompt("main");
+            // Write string of site
+            console.log("Before writing string");
+            canvasConsole.write_string(context2d, string);
+            console.log("After writing string");
 
-    console.log("After setting prompt");
-
-    const data = await get_string_from_file(main_content_file);
-
-    console.log("Before writing string");
-    canvasConsole.write_string(context2d, data);
-
-    console.log("After writing string");
-
+            // Write prompt after
+            console.log("Before setting prompt");
+            canvasConsole.set_prompt("main");
+            console.log("After setting prompt");
+        });
 })();
