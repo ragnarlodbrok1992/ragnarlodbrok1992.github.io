946e815ef4e571945844340ea5e7876b6ca6541d
ragnarlodbrok1992 <maraus92@gmail.com>
2025-07-03 22:03:06 +0200

    WIP

--- RagnarSite.js
+++
@@ -1,4 +0,0 @@
-function MyApp() {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Welcome to MySite!"));
-}
-
--- RagnarSite.jsx
+++
@@ -1,7 +0,0 @@
-function MyApp() {
-  return (
-    <div>
-      <h1>Welcome to MySite!</h1>
-    </div>
-  );
-}
